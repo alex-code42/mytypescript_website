709d7ae79d639eb7b447b6f4344a49f65f04612d
Alex <areichel1986@gmail.com>
2023-10-11 11:19:58 +0200

    yeah

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
 import Collections from '@/components/collections'
 import Navbar from '@/components/navigation'
-import ProductList from '@/components/product'
+import ProductList from '@/components/productlist'
 
 
 export default function Main() {
   return (
     <div>
+      <div className="fixed top-0 left-0 z-50 w-full bg-white shadow-lg">
         <Navbar/>
+      </div>
+      <div className="pt-16">
         <Collections/>
         <ProductList/>
+      </div>
     </div>
      
     
