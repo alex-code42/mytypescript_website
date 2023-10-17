a2c5a1deab9292db3cc45482ece51ded9f353572
Alex <areichel1986@gmail.com>
2023-10-17 14:42:06 +0200

    addingsdf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
-import Collections from '@/components/collections'
-import Navbar from '@/components/navigation'
 import ProductList from '@/components/productlist'
-import { useState } from 'react';
-import ShoppingCart from '@/components/shopping_cart';
+import Collections from '@/components/collections'
+
 
 
 
@@ -11,6 +9,7 @@ export default function MyPage() {
   return (
     <div>
         <ProductList/>
+        <Collections/>
     </div>
      
     
--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,35 +1,14 @@
-import Navbar from "@/components/navigation";
-import ProductDetail from "@/components/product_detail";
-import ShoppingCart from "@/components/shopping_cart";
-import { useState, useEffect  } from 'react'
+
 import Collections from "@/components/collections";
 import ProductList from "@/components/productlist";
-import useOpenShoppingCard from "@/components/zustand";
-
-
-interface Item {
-  id: number;
-  name: string;
-  price: number;
-}
-
 
   
   export default function MyTshirtPage() {
-    const { isTrue, toggleState } = useOpenShoppingCard();
 
-    
-    
     return (
       <div>
       <ProductList/>
-      <div>
-        <button onClick={toggleState}>Toggle Shopping Cart</button>
-        {isTrue ? <p>Shopping Cart is open.</p> : <p>Shopping Cart is closed.</p>}
-      </div>
-
       <Collections/>
-        {/* <ProductDetail addItemToCart={addItemToCart} setOpenCart={localSetOpenCart}/> */}
       </div>
     );
   }
