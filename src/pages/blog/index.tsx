985d3e4d2423ec601a79d62e2e27ddb680075905
Alex <areichel1986@gmail.com>
2023-11-26 16:02:15 +0100

    yeah

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,31 @@
 import TravelBlogPost from "@/components/blog/article";
+import Collections from "@/components/collections";
+import { log } from "console";
+import  { createClient } from 'contentful'
 
-export default function BlogIndexPage() {
+
+export async function getStaticProps() {
+
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID || '',
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY || ''
+  })
+
+  const res = await client.getEntries({ content_type: 'blog' })
+  log("this is the res",res);
+  return {
+
+    props: {
+      blog: res.items
+    }
+  }
+}
+
+export default function BlogIndexPage({blog } : {blog: any}) {
+  console.log("this is the blog-xxx",blog);
   return (
     <div>
-      <TravelBlogPost />
+      <Collections blog={blog}/>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,35 @@
 import ProductList from '@/components/productlist'
 import Collections from '@/components/collections'
+import  { createClient } from 'contentful'
 
 
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID || '',
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY || ''
+  });
 
+  const productRes = await client.getEntries({ content_type: 'product' });
+  const blogRes = await client.getEntries({ content_type: 'blog' });
 
-export default function MyPage() {
-
-  return (
-    <div>
-        {/* <ProductList/> */}
-        <Collections/>
-    </div>
-     
-    
-  )
+  return {
+    props: {
+      productss: productRes.items,
+      blog: blogRes.items
+    }
+  };
 }
+
+
+  
+  export default function MyPage({productss,blog } : {productss: any, blog: any}) {
+    console.log("this is the blogs-zzz",blog);
+    
+    return (
+      <div>
+      <Collections blog={blog}/>
+      <ProductList productss={productss}/>
+      
+      </div>
+    );
+  }
